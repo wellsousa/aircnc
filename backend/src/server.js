bd92bd67769ce8764c50b6cb0635a38faae0a73e
Wellington de Sousa <wellsousa1987@outlook.com>
2019-10-02 01:46:16 -0300

    criação do projeto inicial.

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+//DICA: use 'const' para variaveis que não alterarção o seu valor
+
+//importando a bliblioteca express
+const express = require('express');
+
+//criando o servidor
+const app = express();
+
+/*
+cria uma rota e uma requisição do tipo get
+Nesse exemplo usa-se arrow function mas o codigo poderia ser escrito assim:
+
+app.get('/', function(req, res){
+    return res.send('Hello World');
+});
+
+req - são os dados que o cliente envia para o servidor
+res - é a resposta que o servidor envia para o cliente
+*/
+app.get('/', (req, res) => {
+    return res.json({ message: 'Hello World!'});
+});
+
+//a aplicação será executada na porta 3333
+app.listen(3333);
