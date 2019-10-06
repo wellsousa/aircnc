c6c54dfc853c67032cc82d7bdc53a4cfed87bc95
Wellington de Sousa <wellsousa1987@outlook.com>
2019-10-06 18:44:14 -0300

    tela de login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,42 @@
-import React from 'react';
-import ReactDOM from  'react-dom';
+import React, {useState} from 'react';
+import api from './services/api';
 import './App.css';
 
+import logo from './assets/logo.svg';
+
 function App() {
+
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event){
+    event.preventDefault();
+    
+    const res = await api.post('/sessions', {email});
+
+    console.log(res);
+  }
+
   return (
-    <div className="App">
-      <h2>Hello World</h2> 
+    <div className="container">
+      <img src={logo} alt="AirCnC"></img>
+
+      <div className="content">
+          <p>
+              Oferaça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
+          </p>
+
+          <form onSubmit={handleSubmit}>
+              <label htmlFor="email">E-MAIL*</label>
+              <input 
+                type="email"
+                id="email"
+                placeholder="Seu melhor e-mail"
+                onChange={event => setEmail(event.target.value)}
+              ></input>
+
+              <button className="btn" type="submit">Entrar</button>
+          </form>
+      </div>
     </div>
   );
 }
