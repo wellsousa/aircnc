84a0715c5ec2807417f3a2ddc85efd05d5cb6e3d
Wellington de Sousa <wellsousa1987@outlook.com>
2019-10-06 10:32:49 -0300

    Estrutura inicial da parte frontend do projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from  'react-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello World</h2> 
+    </div>
+  );
+}
+
+export default App;
